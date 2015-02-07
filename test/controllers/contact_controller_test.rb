aca499ba185a5170f3d57e126ee6403cbca69ab2
tmf <tom.forrer@gmail.com>
2015-02-07 15:18:13 +0100

    contact page controller, with page title

--- test/controllers/contact_controller_test.rb
+++ test/controllers/contact_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class ContactControllerTest < ActionController::TestCase
-  test "should get list" do
-    get :list
-    assert_response :success
-  end
+
+    def setup
+        @base_title = "tomforrer.ch"
+    end
+
+    test "should get list" do
+      get :list
+      assert_response :success
+      assert_select "title", "Contact - #{@base_title}"
+    end
 
 end
